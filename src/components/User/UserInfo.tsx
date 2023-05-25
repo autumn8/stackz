0aa8ced6546a3bf1c6e5878d98ac7e541b0e63d5
Brad Roodt <brad.roodt@gmail.com>
2023-05-25 15:19:20 +0200

    feat: adds transition & basic click handlers for actions

--- src/components/User/UserInfo.tsx
+++ src/components/User/UserInfo.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent, ReactElement, ReactFragment, useState } from "react";
 import { User } from "./user.interface";
 import "./UserInfo.scss";
 
@@ -6,15 +7,40 @@ interface UserInfoProps {
 }
 
 const UserInfo = ({ user }: UserInfoProps) => {
+  const [showActionBar, setShowActionBar] = useState(false);
+
+  const toggleUserActions = (event: MouseEvent): void => {
+    console.log("toggle actions");
+    setShowActionBar(!showActionBar);
+  };
+
+  const toggleFollowUser = (event: MouseEvent): void => {
+    console.log("toggle follow");
+    if (event.target === event.currentTarget) {
+      event.stopPropagation();
+    }
+  };
+
+  const blockUser = (event: MouseEvent) => {
+    console.log("block user");
+    if (event.target === event.currentTarget) {
+      event.stopPropagation();
+    }
+  };
+
   return (
-    <div className="user-info">
+    <div className="user-info" onClick={toggleUserActions}>
       <img src={user.profileImage} className="user-info__image" />
       <div className="user-info__content">
         <h1 className="user-info__name">{user.displayName}</h1>
         <h2 className="user-info__reputation">{user.reputation}</h2>
-        <div className="user-info__actions">
-          <button>Follow</button>
-          <button>Block</button>
+        <div
+          className={`user-info__actions ${
+            showActionBar ? "user-info__actions--active" : ""
+          }`}
+        >
+          <button onClick={toggleFollowUser}>Follow</button>
+          <button onClick={blockUser}>Block</button>
         </div>
       </div>
     </div>
